031249fa276bdcb4cfb5dc9dc625814a2759f5ca
Hernando Dio Palma <hernando.diopalma@gmail.com>
2024-03-17 00:10:41 +0700

    Update participant table loading animation

--- src/components/CardBoxComponentLoading.vue
+++ src/components/CardBoxComponentLoading.vue
@@ -22,54 +22,62 @@ export default {
       type: Boolean,
       default: false
     }
+  },
+  computed: {
+    cssProps() {
+      return {
+        '--animation-duration': this.duration
+      }
+    }
+  },
+  methods: {
+    getAnimationDelay(index) {
+      const delay = parseFloat(this.duration) / 10;
+      return `animation-delay: -${delay * index}s`;
+    }
   }
 }
 </script>
 
 <template>
-<div class='flex space-x-2 justify-center items-center text-gray-500 dark:text-gray-400' :style="{'--animation-duration': duration}" :class="`${height} ${padding} ${bold ? 'font-bold' : ''}`">
- 	<span class='sr-only'>Loading...</span>
-  
-<div class='h-4 w-4 animate-bounce [animation-delay:0s] '>L</div>
-<div class='h-4 w-4 animate-bounce [animation-delay:-0.15s] '>O</div>
-<div class='h-4 w-4 animate-bounce [animation-delay:-0.3s] '>A</div>
-<div class='h-4 w-4 animate-bounce [animation-delay:-0.45s] '>D</div>
-<div class='h-4 w-4 animate-bounce [animation-delay:-0.6s] '>I</div>
-<div class='h-4 w-4 animate-bounce [animation-delay:-0.75s] '>N</div>
-<div class='h-4 w-4 animate-bounce [animation-delay:-0.9s] '>G</div>
-<div class='h-4 w-4 animate-bounce [animation-delay:-1.05s] '>.</div>
-<div class='h-4 w-4 animate-bounce [animation-delay:-1.2s] '>.</div>
-<div class='h-4 w-4 animate-bounce [animation-delay:-1.35s] '>.</div>
+  <div class='flex space-x-2 justify-center items-center text-gray-500 dark:text-gray-400'
+    :class="`${height} ${padding} ${bold ? 'font-bold' : ''}`"
+    :style="cssProps">
+    <span class='sr-only'>Loading...</span>
 
-</div>
-
+    <div v-for="(letter, index) in ['L', 'O', 'A', 'D', 'I', 'N', 'G']"
+      :key="index"
+      class='h-4 w-4 animate-bounce'
+      :style="getAnimationDelay(index)">
+      {{ letter }}
+    </div>
+  </div>
 </template>
 
 <style>
-  .animate-bounce {
-    animation-duration: var(--animation-duration, 2.5s);
-    animation-iteration-count: infinite;
-    animation-timing-function: ease-in-out;
-    transform-origin: center bottom;
-    animation-name: fadeInOut;
+.animate-bounce {
+  animation-duration: var(--animation-duration);
+  animation-iteration-count: infinite;
+  animation-timing-function: ease-in-out;
+  transform-origin: center bottom;
+  animation-name: fadeInOut;
+}
+
+/* Translate X fade in and out */
+@keyframes fadeInOut {
+  0% {
+    transform: translateX(-20px);
+    opacity: 0;
   }
 
-  /* Translate X fade in and out */
-  @keyframes fadeInOut {
-    0% {
-      transform: translateX(-20px);
-      opacity: 0;
-    }
-    50% {
-      transform: translateX(0);
-      opacity: 1;
-    }
-    100% {
-      transform: translateX(20px);
-      opacity: 0;
-    }
+  50% {
+    transform: translateX(0);
+    opacity: 1;
   }
 
-  
-
+  100% {
+    transform: translateX(20px);
+    opacity: 0;
+  }
+}
 </style>
