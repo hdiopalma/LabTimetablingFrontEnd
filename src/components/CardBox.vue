637484c9540234a062aa912c6a11a7841b22a9bf
Hernando Dio Palma <hernando.diopalma@gmail.com>
2024-05-05 23:08:04 +0700

    Make CardBox to allow nested Card.

--- src/components/CardBox.vue
+++ src/components/CardBox.vue
@@ -21,7 +21,12 @@ const props = defineProps({
   isHoverable: Boolean,
   isModal: Boolean,
   isScrollable: Boolean,
-  wide: Boolean
+  wide: Boolean,
+  isNested: Boolean,
+  nestedLevel: {
+    type: Number,
+    default: 0
+  }
 })
 
 const emit = defineEmits(['submit'])
@@ -30,12 +35,19 @@ const slots = useSlots()
 
 const hasFooterSlot = computed(() => slots.footer && !!slots.footer())
 
+//Manage bg color based on nested level, if even then slightly darker, if odd then regular, if zero then regular
+const bgClass = computed(() => {
+  return props.isNested ? (props.nestedLevel % 2 === 1 ? 'dark:bg-slate-900/70 bg-slate-100' : 'dark:bg-slate-900/70 bg-white') : 'dark:bg-slate-900/60 bg-white'
+})
+
 const componentClass = computed(() => {
   const base = [
     props.rounded,
     props.flex,
-    props.isModal ? 'dark:bg-slate-900' : 'dark:bg-slate-900/70',
-    props.isScrollable ? 'overflow-y-auto' : 'overflow-hidden'
+    props.isModal ? 'dark:bg-slate-900' : bgClass.value,
+    props.isScrollable ? 'overflow-y-auto' : 'overflow-hidden',
+    props.isNested ? `ml-${props.nestedLevel}` : '',
+    props.isNested ? 'border-l-4 dark:border-slate-800' : '',
   ]
 
   if (props.isHoverable) {
