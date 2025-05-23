f160a2bffce679faa48dac81b9dc10a9aed07dd8
Hernando Dio Palma <hdiopalma@gmail.com>
2025-05-23 10:57:17 +0700

    Enhance UserCard component: implement dynamic last login time display and update greeting message

--- src/components/UserCard.vue
+++ src/components/UserCard.vue
@@ -11,8 +11,40 @@ import PillTag from '@/components/PillTag.vue'
 const mainStore = useMainStore()
 
 const userName = computed(() => mainStore.userName)
+const userEmail = computed(() => mainStore.userEmail)
+const lastLogin = computed(() => {
+  const lastLoginDate = mainStore.userLastLogin
+  if (lastLoginDate) {
+    // return new Date(lastLoginDate).toLocaleString()
+    // format is (time) minute/hours/days ago, if more than 30 days, return date
+    const now = new Date()
+    const diff = now - new Date(lastLoginDate)
+    const diffInMinutes = Math.floor(diff / (1000 * 60))
+    const diffInHours = Math.floor(diff / (1000 * 60 * 60))
+    const diffInDays = Math.floor(diff / (1000 * 60 * 60 * 24))
+    const diffInMonths = Math.floor(diff / (1000 * 60 * 60 * 24 * 30))
+    const diffInYears = Math.floor(diff / (1000 * 60 * 60 * 24 * 365))
+    if (diffInMinutes < 60) {
+      return `${diffInMinutes} minutes ago`
+    } else if (diffInHours < 24) {
+      return `${diffInHours} hours ago`
+    } else if (diffInDays < 30) {
+      return `${diffInDays} days ago`
+    } else if (diffInMonths < 12) {
+      return `${diffInMonths} months ago`
+    } else if (diffInYears > 1) {
+      return new Date(lastLoginDate).toLocaleDateString()
+    }
+  }
+  return 'Never'
+})
+
 
 const userSwitchVal = ref(false)
+
+//last login
+
+
 </script>
 
 <template>
@@ -30,12 +62,14 @@ const userSwitchVal = ref(false)
           />
         </div>
         <h1 class="text-2xl">
-          Howdy, <b>{{ userName }}</b
+          Hello, <b>{{ userName }}</b
           >!
         </h1>
-        <p>Last login <b>12 mins ago</b> from <b>127.0.0.1</b></p>
+        <p>Last login <b>
+          {{ lastLogin }}
+        </b> from <b>127.0.0.1</b></p>
         <div class="flex justify-center md:block">
-          <PillTag label="Verified" color="info" :icon="mdiCheckDecagram" />
+          <!-- <PillTag label="Verified" color="info" :icon="mdiCheckDecagram" /> -->
         </div>
       </div>
     </BaseLevel>
